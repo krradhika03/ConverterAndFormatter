c1a8aa459ed2f3657b9bb580f138bca5b0fc2aa1
krradhika03 <krradhika03@gmail.com>
2023-04-12 13:02:36 -0400

    async/await call

--- src/app/converter-and-formatter/converter-and-formatter/converter/converter/converter.component.ts
+++ src/app/converter-and-formatter/converter-and-formatter/converter/converter/converter.component.ts
@@ -1,7 +1,8 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import * as JsonToXML from "js2xmlparser";
-import { NgxSpinnerService } from "ngx-spinner";  
+import { NgxSpinnerService } from "ngx-spinner";
+import { take } from 'rxjs';
 
 interface Format {
   name: string,
@@ -71,11 +72,18 @@ export class ConverterComponent implements OnInit {
 
   }
 
+  private delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  async getDaoResponseAndWait(fileName: string) {
+    let result = await this.getDaoRetrieve(fileName);
+    return result;
+  }
   upload() {
     console.log('test');
-//    const file: File = event.target.files[0];
-    if (this.file) {
-      this.fileName = this.file.name;
+    //    const file: File = event.target.files[0];
+    if (this.fileName) {
+      // this.fileName = JSON.parse(JSON.stringify(this.file.filename));
       const formData = new FormData();
       formData.append("thumbnail", this.file);
       const httpOptions = {
@@ -83,27 +91,57 @@ export class ConverterComponent implements OnInit {
 
           "ContentType": "multipart/form-data",
           "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+          "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+
+
 
         })
       }
-    this.SpinnerService.show();
-     this.http.post<any>("https://us-central1-visionapidemo-381801.cloudfunctions.net/processUploadLabel", formData, httpOptions).
-      subscribe(res=>{
-          if(res)
-          {
-            this.validList= res.valid;
-            this.invalidList = res.invalid;
-          }
+      this.SpinnerService.show();
+      this.http.post<String>("https://us-central1-visionapidemo-381801.cloudfunctions.net/processUploadLabel", formData, httpOptions).subscribe(async res => {
+        console.log(JSON.stringify(res));
+        if (res) {
+          console.log(res)
+          var count = 1;
+          var responseDao;
+          do {
+            if (count > 1) {
+              await this.delay(10000);
+            }
+            responseDao = await this.getDaoResponseAndWait(this.fileName);
+            console.log(responseDao)
+            count++;
+          } while (responseDao == null || count > 5);
+
           this.SpinnerService.hide();
+          res = responseDao;
+          if (responseDao) {
+            this.validList = responseDao.valid;
+            this.invalidList = responseDao.invalid;
+          }
+        }
       });
     }
-    }
+  }
 
-    onUpload(event:any)
-    {
-      
-    this.file= event.target.files[0];
-   
+  onUpload(event: any) {
+
+    this.file = event.target.files[0];
+    this.fileName = event.target.files[0].name;
+
+  }
+  getDaoRetrieve(fileName: string): Promise<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "ContentType": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+
+      })
     }
+    console.log(fileName.toUpperCase())
+    return this.http.post<any>("https://us-central1-visionapidemo-381801.cloudfunctions.net/daoRetrieve", fileName.toUpperCase(), httpOptions).pipe(take(1)).toPromise();
+  }
+
 }
+
